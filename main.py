dc42b32b5c2b00b8bce13e8698998133953bf380
Tomás Capretto <elmagodegit@gmail.com>
2026-06-30 12:42:03 +0300

    Agrego programa que genera graficos

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import polars as pl
+from plotnine import ggplot, geom_point, aes
+
 def main():
-    print("Hello from repo-rebase!")
+    df = pl.read_csv("datos/penguins.csv")
+    grafico = ggplot(df) + geom_point(aes(x="bill_length_mm", y="bill_depth_mm"))
+    grafico.save("plot.png")
 
 
 if __name__ == "__main__":
